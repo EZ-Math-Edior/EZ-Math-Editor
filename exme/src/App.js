b26c8e940ce608f6d4cb23813541f29015baf6ba
Shroooom <75349975+Shroooom@users.noreply.github.com>
2021-06-30 21:20:45 -0400

    made Generate PDF button and hideable buttons
    npm install jspdf --save
    ^ for pdf generation
    
    pdf generator currently only grabs text from the static text box

--- exme/src/App.js
+++ exme/src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import HideableText from './HideableText';
 import TextField from './TextField'
 import DraggableField from './DraggableField';
+import jsPDF from 'jspdf';
 /****************************************
 * TODO:
 *	Stop editable text box from infinitely resizing
@@ -17,11 +18,12 @@ import DraggableField from './DraggableField';
 * change state of child from parent (aka app) https://www.freecodecamp.org/news/react-changing-state-of-child-component-from-parent-8ab547436271/
 ****************************************/
 
-class App extends React.Component{
+class App extends React.PureComponent{
 	constructor(props){
 		super(props);
-		this.lockElement = React.createRef(); //associates a jsx element with the component itself i think? 
+		this.lockElement = React.createRef(); //associates a jsx element with the component itself i think?
 											//i.e. we can access a different component's member functions
+		this.state = {}
 	}
 
 	sayHello = () => {
@@ -30,35 +32,62 @@ class App extends React.Component{
 		alert("in button 1");
 	}
 
-	lockTextBox = () => { 
+	lockTextBox = () => {
 		this.lockElement.current.lockReverse();
 
 	};
 
+	// generate pdf function
+	generatePDF = () => {
+		// new doc variable
+		var doc = new jsPDF('p', 'pt', 'a4');
+		doc.html(document.querySelector("#editor"), {
+			callback: function(pdf) {
+				pdf.save("yourPDF.pdf");
+			}
+		}
+	);
+	}
+
 	render() {
 		return (
+
 			<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
 				<p>
 				WELCOME TO THE EZ MATH EDITOR!
-				<HideableText text = "Bold italiasda underlien"/>
+
+				<HideableText text = <p>
+						<button onClick={this.sayHello}>bold</button>
+						<button onClick={this.sayHello}>italicize</button>
+						<button onClick={this.sayHello}>underline</button>
+														 </p>/>
 
 				</p>
 				<div id ="editor">
 					<p>
-						<TextField/> 
+						<TextField/>
 					</p>
 					<p>
 						<DraggableField ref={this.lockElement}/> {/* this is how you associate a jsx element with the createRef in the constructor */}
 					</p>
 
-					
 				</div>
+
+				<div id ="testtext">
+					<p>
+						<TextField/>
+					</p>
+				</div>
+
 				<div class="btn-group">
 					<button onClick={this.sayHello}>Btn1</button>
 					<button onClick={this.lockTextBox}>Lock field</button>
 					<button>Btn3</button>
+
+					<button onClick={this.generatePDF} type="primary">get your pdf</button>
+
 				</div>
 				</header>
 			</div>
